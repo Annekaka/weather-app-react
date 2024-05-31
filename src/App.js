cee6563d7fe6d0d7ba898ca4aac3842e323b974c
Annekaka <97798005+Annekaka@users.noreply.github.com>
2024-05-31 21:33:30 +0200

    Added links

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <CurrentWeather />
-      <Forcast />
+    <div>
+      <div className="App">
+        <CurrentWeather />
+        <Forcast />
+      </div>
+      <footer>
+        This project was coded by <strong>Anne Helene Nergård</strong> and is
+        open sourced on
+        <a href="https://github.com/Annekaka/weather-app-react"> Github</a>.
+        Hosted on
+        <a href="https://magnificent-strudel-e35b8f.netlify.app"> Netlify</a>
+      </footer>
     </div>
   );
 }
